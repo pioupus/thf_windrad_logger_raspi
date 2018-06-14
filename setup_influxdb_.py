b156c68eb0aaeb4fa4b7895cd6652032b5ca0039
arne <raspi@example.com>
2018-06-14 20:59:38 +0000

    test

--- setup_influxdb_.py
+++ setup_influxdb_.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python 
 from influxdb import InfluxDBClient
 client = InfluxDBClient('localhost', 8086, 'root', 'root', 'example')
-client.create_database('thf_logger')
+client.create_database('thf_logger')#bump
