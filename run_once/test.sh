bee2d27c46447bbffb5a21d5da9d6a38736b5fe4
Arne Krüger <kruegerarneak@gmail.com>
2018-06-16 16:17:20 +0200

    adjusted paths

--- run_once/test.sh
+++ run_once/test.sh
@@ -1,2 +1,5 @@
  #!/bin/sh
- echo hallo ich bin ein shell script, nun nummer 2
+echo hallo ich bin ein shell script, nun nummer 2
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+echo $SCRIPTPATH
