93dcb1658873f02524196cee2f1d436dab42f6e1
Arne Krüger <kruegerarneak@gmail.com>
2018-06-16 13:31:29 +0200

    cron job

--- run_once/install_update_cron_job.sh
+++ run_once/install_update_cron_job.sh
@@ -2,6 +2,6 @@
  
 croncmd="python /home/pi/thf_windrad_logger_raspi/update/do_update.py > /home/pi/update_cron_log.log 2>&1"
 
-cronjob="0 */15 * * * $croncmd"
+cronjob="*/15 * * * * $croncmd"
 
 ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab - 
