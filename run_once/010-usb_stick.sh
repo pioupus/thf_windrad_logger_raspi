adc7084e79acb13d3b22873d6b0e7ac66f6f411d
Arne Krüger <kruegerarneak@gmail.com>
2018-06-16 19:24:21 +0200

    usb stick installer

--- run_once/010-usb_stick.sh
+++ run_once/010-usb_stick.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+echo $MY_PASSWORD | sudo -S mkdir /media/
 echo $MY_PASSWORD | sudo -S mkdir /media/usbstick
 echo $MY_PASSWORD | sudo -S chmod 775 /media/usbstick
-echo $MY_PASSWORD | sudo -S echo LABEL=logger_usb_stick  /media/usbstick/ ext2   defaults 0 0 >> /etc/fstab
+echo $MY_PASSWORD | sudo -S bash -c 'echo LABEL=logger_usb_stick  /media/usbstick/ ext2   defaults 0 0 >> /etc/fstab'
 
 
