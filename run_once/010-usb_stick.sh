393e97bb12aabc8077c57449a637f089af333ac3
Arne Krüger <kruegerarneak@gmail.com>
2018-06-17 12:29:44 +0200

    isntalls influxdb

--- run_once/010-usb_stick.sh
+++ run_once/010-usb_stick.sh
@@ -2,7 +2,7 @@
 
 echo $MY_PASSWORD | sudo -S mkdir /media/
 echo $MY_PASSWORD | sudo -S mkdir /media/usbstick
-echo $MY_PASSWORD | sudo -S chmod 775 /media/usbstick
-echo $MY_PASSWORD | sudo -S bash -c 'echo LABEL=logger_usb_stick  /media/usbstick/ ext2   defaults 0 0 >> /etc/fstab'
+echo $MY_PASSWORD | sudo -S chmod 777 /media/usbstick
+echo $MY_PASSWORD | sudo -S bash -c 'echo LABEL=logger_usb_stick  /media/usbstick/        ext2    rw,user,exec,sync,auto,nofail   0       2 >> /etc/fstab'
 
 
---
+++ run_once/020_install_influxdb.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+echo $MY_PASSWORD | sudo -S apt install apt-transport-https
+echo "deb https://repos.influxdata.com/debian stretch stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+echo $MY_PASSWORD | sudo -S apt update
+echo $MY_PASSWORD | sudo -S apt-get install influxdb
+echo $MY_PASSWORD | sudo -S cp -rf $SCRIPTPATH/../etc/influxdb/influxdb.conf /etc/influxdb/influxdb.conf
+#sudo service influxdb start
