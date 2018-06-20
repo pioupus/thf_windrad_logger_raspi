f4dc020a3f0ff7862c7c0c139e7c906d82cfb0f7
Arne Krüger <kruegerarneak@gmail.com>
2018-06-20 22:05:11 +0200

    updates

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # How To
 - Install raspbian lite on SD-Card
+- add the line 'enable_uart=1' to /boot/config.txt
+- delete 'console=serial0,115200' from cmdline.txt
+- put the file 'ssh' in the /boot/ directory for enabling ssh
 - add the line
 
 ```
@@ -7,5 +10,8 @@ export MY_PASSWORD=xxx
 ```
 to ~/.bashrc where xxx is your ssh sudo password
 
-- clone this repository
-- execute ./thf_windrad_logger/update/do_update.py
+- run sudo apt-get update
+- install git using sudo apt-get install git
+- clone this repository using git clone https://github.com/pioupus/thf_windrad_logger_raspi.git
+- cd thf_windrad_logger/update/
+- execute ./run_run_once.py
--- update/run_run_once_files.py
+++ update/run_run_once_files.py
@@ -17,20 +17,26 @@ print(files_which_already_ran)
 
 files_i_run = []
 
+files_to_run = []
+
 for filename in os.listdir(directory):
     if filename.endswith(".sh") or filename.endswith(".py"):
 
         full_path = os.path.join(directory, filename)
         isexecutable = os.access(full_path, os.X_OK)
         if isexecutable:
-            if full_path in files_which_already_ran:
-                print("Skipping: "+full_path)
-                continue
-            
-            print("Running: "+full_path)
-            stdoutput = subprocess.Popen(full_path, shell=True,stdout=subprocess.PIPE).stdout.read()
-            print(stdoutput)
-            files_which_already_ran.append(full_path)
+            files_to_run.append(full_path)
+
+files_to_run.sort()
+for full_path in files_to_run:
+    if full_path in files_which_already_ran:
+        print("Skipping: "+full_path)
+        continue
+
+    print("Running: "+full_path)
+    stdoutput = subprocess.Popen(full_path, shell=True,stdout=subprocess.PIPE).stdout.read()
+    print(stdoutput)
+    files_which_already_ran.append(full_path)
 
 print(files_which_already_ran)
 
