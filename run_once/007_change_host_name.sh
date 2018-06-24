161b4d2d8d0af44d98cc921e7e9d1a8203f78a18
Arne Krüger <kruegerarneak@gmail.com>
2018-06-24 23:36:35 +0200

    hostname

--- run_once/007_change_host_name.sh
+++ run_once/007_change_host_name.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo $MY_PASSWORD | sudo -S python 007_change_host_name.py
-echo $MY_PASSWORD | sudo -S iconfig eth0 down
-echo $MY_PASSWORD | sudo -S iconfig eth0 up
+echo $MY_PASSWORD | sudo -S ifconfig eth0 down
+echo $MY_PASSWORD | sudo -S ifconfig eth0 up
