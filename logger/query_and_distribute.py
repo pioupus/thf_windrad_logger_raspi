d8f09482f2fc1551177634a49dc36882f39493e7
Arne Krüger <kruegerarneak@gmail.com>
2018-06-28 19:19:41 +0200

    first influx query tests

--- logger/query_and_distribute.py
+++ logger/query_and_distribute.py
@@ -19,7 +19,7 @@ if os.path.isfile(LAST_TIME_STAMP_FN):
 while 1:
     logger_data = client.query("SELECT * FROM powerdata WHERE logger_time > "+str(last_time_stamp))
     for data_set in logger_data: 
-        print(logger_data)
+        print(data_set)
         last_time_stamp = data_set['logger_time']
     with open(LAST_TIME_STAMP_FN, 'w') as last_time_stamp_file:
         last_time_stamp_file.write(str(last_time_stamp))
