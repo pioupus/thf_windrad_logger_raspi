818598633692bb60f697bdda9938a84b54fa1f0d
Arne Krüger <kruegerarneak@gmail.com>
2018-06-28 21:44:05 +0200

    test display

--- logger/pyRPCProtocol.py
+++ logger/pyRPCProtocol.py
@@ -218,11 +218,7 @@ arguments_sys_stat["row"] = 0
 arguments_sys_stat["screen_index"] = 0
 arguments_sys_stat["text_in"] = string_to_ord("IP: "+str(my_IP_address),20)
 
-while 1:
-    result = proto.call("display_set_sysstat_screen",arguments_sys_stat)
-    print(result)
-    time.sleep(0.5)
-    
+result = proto.call("display_set_sysstat_screen",arguments_sys_stat)
 #time.sleep(1)
 arguments_get_adc_values = {}
 result = proto.call("get_device_descriptor",arguments_get_adc_values)
