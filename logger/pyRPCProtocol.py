91555fdfd6c367a19dee8766525e5cf1a70e87be
Arne Krüger <kruegerarneak@gmail.com>
2018-07-22 18:55:17 +0200

    timestamp

--- logger/pyRPCProtocol.py
+++ logger/pyRPCProtocol.py
@@ -295,7 +295,7 @@ while 1:
         
     duration = (time.clock() - start_time)*1000
     logger_unix_time = float(result["arguments"]["unix_time"])
-    sub_seconds = float(result["arguments"]["sub_seconds"]))
+    sub_seconds = float(result["arguments"]["sub_seconds"])
     print("logger_unix_time: "+str(logger_unix_time))
     print("sub_seconds: "+str(sub_seconds)
     logger_unix_time = logger_unix_time + (sub_seconds/256.0)
