db2c4e216c1519ba01aef1e3bb35f3b63c065938
Arne Krüger <kruegerarneak@gmail.com>
2018-10-20 22:29:51 +0200

    added backup

--- run_once/install_update_cron_job.sh
+++ run_once/install_update_cron_job.sh
@@ -1,7 +1,11 @@
  #!/bin/sh
  
-croncmd="python /home/pi/thf_windrad_logger_raspi/update/do_update.py > /home/pi/update_cron_log.log 2>&1"
+croncmd_update="python /home/pi/thf_windrad_logger_raspi/update/do_update.py > /home/pi/update_cron_log.log 2>&1"
+cronjob_update="*/15 * * * * croncmd_update"
 
-cronjob="*/15 * * * * $croncmd"
 
-( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab - 
+croncmd_backup="/home/pi/thf_windrad_logger_raspi/update/backup.sh"
+cronjob_backup="*/15 * * * * croncmd_backup"
+
+( crontab -l | grep -v -F "croncmd_update" ; echo "cronjob_update" ) | crontab - 
+( crontab -l | grep -v -F "croncmd_backup" ; echo "cronjob_backup" ) | crontab - 
